8af5327c6bd779b62975c561177dd5b0578ecc7a
Matt Plichta <bigcatplichta@gmail.com>
2019-08-14 21:28:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -7,7 +7,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  quotient = num2 / num1
+  quotient = num1 / num2
 end
 
 def multiplication(num1, num2)
@@ -21,5 +21,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-  math.sqrt(num)
+  square_root = math.sqrt(num)
 end
